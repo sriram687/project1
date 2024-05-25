f07bb856b54193b125b87d3d0676ffbc5bdeb94c
sriram687 <msram2006@gmail.com>
2024-05-25 15:16:41 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import {default as Article} from "./article/Article"
+import {default as Brand} from "./brand/Brand"
+import {default as CTA} from "./cta/CTA"
+import {default as Feature} from "./feature/Feature"
+import {default as Navbar} from "./navbar/Navbar"
